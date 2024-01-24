aa1d5e6cf166b92d32dcfdf433e1e643eccb07a3
Lucas <gasparlucas8@gmail.com>
2024-01-23 22:22:17 -0300

    Dia 2

--- dia-2/main.js
+++ dia-2/main.js
@@ -1,3 +1,4 @@
+// Consignas
 // 1 - Tengo una lista de productos para comprar, pero tengo que cambiar el orden: tiene que ser de menos caracteres a mayores
 // 2 - También tengo que agregar 2 productos, uno al inicio y otro al final.
 // 3 - Pero me arrepentí, quiero sacar el último.
@@ -38,3 +39,51 @@ console.log("---------------PUNTO 3-----------")
 
 listaComprasOrdenada.pop()
 console.log(listaComprasOrdenada)
+
+// PUNTO 4
+console.log("------------------PUNTO 4----------------")
+
+// defino el objeto con un atributo frutas, que es un array de objetos con las keys nombre y cantidad, sin valores. 
+const listaDeCompras = {
+    frutas: [{
+        nombre: "",
+        cantidad: null
+    },
+    {
+        nombre: "",
+        cantidad: null
+    },
+    {
+        nombre: "",
+        cantidad: null
+    },
+    {
+        nombre: "",
+        cantidad: null
+    },
+    {
+        nombre: "",
+        cantidad: null
+    }
+]
+}
+// Asignar valor a las keys nombre
+// listaDeCompras.frutas[0].nombre = listaComprasOrdenada[0]
+// listaDeCompras.frutas[1].nombre = listaComprasOrdenada[1]
+// listaDeCompras.frutas[2].nombre = listaComprasOrdenada[2]
+// listaDeCompras.frutas[3].nombre = listaComprasOrdenada[3]
+// listaDeCompras.frutas[4].nombre = listaComprasOrdenada[4]
+
+// Asignar valor a las keys cantidad
+// listaDeCompras.frutas[0].cantidad = listaComprasOrdenada[0].length
+// listaDeCompras.frutas[1].cantidad = listaComprasOrdenada[1].length
+// listaDeCompras.frutas[2].cantidad = listaComprasOrdenada[2].length
+// listaDeCompras.frutas[3].cantidad = listaComprasOrdenada[3].length
+// listaDeCompras.frutas[4].cantidad = listaComprasOrdenada[4].length
+
+// Asigno valores a las keys
+for (let i = 0; i < 5; i++) {
+    listaDeCompras.frutas[i].nombre = listaComprasOrdenada[i]
+    listaDeCompras.frutas[i].cantidad = listaComprasOrdenada[i]
+}
+console.log(listaDeCompras)
